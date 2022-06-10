f8bc72edbaace2befd48c2c5e754972e837c13a0
Filip Todorovic <filip.tod95@gmail.com>
2022-06-10 11:00:53 +0200

    .

---
+++ css.hr/python.py
@@ -0,0 +1,8 @@
+weight = int(input("weight "))
+unit = input("(L)bs or (K)g: ")
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted}kilos")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print(f"You are {converted}pounds  ")
